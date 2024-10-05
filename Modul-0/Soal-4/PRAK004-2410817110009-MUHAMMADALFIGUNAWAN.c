6a09315e5b9c2b5901bc7ffa92a38676fb85792a
Gitarpetir <alfigunawan291205@gmail.com>
2024-10-05 18:58:07 +0800

    Add files via upload

---
+++ Modul-0/Soal-4/PRAK004-2410817110009-MUHAMMADALFIGUNAWAN.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf ("##############################\n");
+    printf ("#                            #\n");
+    printf ("#    Muhammad Alfi Gunawan   #\n");
+    printf ("#        2410817110009       #\n");
+    printf ("#                            #\n");
+    printf ("##############################");
+
+}
